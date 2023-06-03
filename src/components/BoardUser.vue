347ada8782f48af53d00615086378b6ca1635fdf
Yuri Merkury <yg@trueflip.io>
2023-06-03 10:08:06 +0300

    games project

--- src/components/BoardUser.vue
+++ src/components/BoardUser.vue
@@ -1,36 +1,39 @@
 <template>
   <div class="container-fluid">
-    <header>
-      <h3>Board</h3>
-      <h3>{{ content }}</h3>
-    </header>
+      <div class="row user-room-iframe" v-if="iframeUrl">
+          <iframe :src="iframeUrl"></iframe>
+      </div>
   </div>
 </template>
 
+<style lang="scss">
+@import "@/assets/styles/user-room.scss";
+</style>
+
 <script>
-// import UserService from "../services/user.service";
+import UserService from "@/services/user.service";
 
 export default {
   name: "board-user",
   data() {
     return {
-      content: "",
+      iframeUrl: "",
     };
   },
   mounted() {
-    // UserService.getUserBoard().then(
-    //   (response) => {
-    //     this.content = response.data;
-    //   },
-    //   (error) => {
-    //     this.content =
-    //       (error.response &&
-    //         error.response.data &&
-    //         error.response.data.message) ||
-    //       error.message ||
-    //       error.toString();
-    //   }
-    // );
+    UserService.getUserBoard().then(
+      (response) => {
+        this.iframeUrl = response.data;
+      },
+      (error) => {
+        this.content =
+          (error.response &&
+            error.response.data &&
+            error.response.data.message) ||
+          error.message ||
+          error.toString();
+      }
+    );
   },
 };
 </script>
